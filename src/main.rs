11f023076a36272c64913da9eecd1eab389f57d2
Laurent Thévenoux <lrnt@thvnx.com>
2019-12-10 21:09:47 +0100

    Build a git branch name from command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::env;
+use gumdrop::Options;
+
+#[derive(Debug, Options)]
+struct KitOptions {
+    #[options(help = "print help message")]
+    help: bool,
+
+    #[options(short = 'd', help = "devel branch")]
+    devel_branch: bool,
+
+    #[options(help = "branch destination")]
+    branch_destination: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = KitOptions::parse_args_default_or_exit();
+
+    println!("{:#?}", opts);
+
+    let branch_type = if opts.devel_branch {
+        "dev"
+    } else {
+        "aci"
+    };
+
+    let user = match env::var("USER") {
+        Ok(val) => val,
+        Err(_e) => "lthevenoux".to_string(),
+    };
+
+    let branch_dest = match opts.branch_destination {
+        None => "csw_coolidge".to_string(),
+        Some(b) => b
+    };
+
+    println!("{}/{}/{}", branch_type, user, branch_dest);
 }
